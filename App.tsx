ebed5967a2cb40e7844d6febefff3aa42e5137c5
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-09-23 22:04:53 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  useEffect(()=> {
+    console.warn("Foi")
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Olá, mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
